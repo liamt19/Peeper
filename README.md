888bff28cb4fd205bcb692399ae5e7e3c56f3bff
Liam McGuire <liam.t.mcguire@gmail.com>
2025-03-05 17:16:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1 align="center">
+Peeper - A USI engine
+</h1>
---
+++ Util/Utilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peeper.Utilities
+{
+    public static class Utilities
+    {
+    }
+}
